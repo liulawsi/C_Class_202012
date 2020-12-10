3c19c7283a845c24d3988856cdee3e2ba6bc69b1
liulawsi <liulawsi@gmail.com>
2020-12-10 16:59:48 +0800

    2020/12/10

---
+++ Section01/SayHi/main.c
@@ -0,0 +1,13 @@
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main()
+ {
+     char name[50];
+
+     printf("你是誰？");
+     scanf("%s", name);
+     printf("Hi~ %s 你好啊～\n", name);
+     return 0;
+ }
