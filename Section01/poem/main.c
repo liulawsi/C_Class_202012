791885edf3ee2936ca5b651a31828d558b73c484
liulawsi <liulawsi@gmail.com>
2020-12-10 16:44:09 +0800

    2020/12/10

---
+++ Section01/poem/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\t床前明月光  疑似地上霜\n");
+	printf("\t舉頭望明月  低頭思便當\n");
+	printf("\t\t\t李黑 靜夜思\n\n");
+	printf("  低  舉  疑  床\n");
+	printf("  頭  頭  似  前  李\n");
+	printf("  思  望  地  明  黑\n");
+	printf("  便  明  上  月\n");
+	printf("  當  月  霜  光  靜\n");
+	printf("                  夜\n");
+	printf("                  思\n");
+    return 0;
+}
