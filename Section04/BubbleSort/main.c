336b216399c6352455254660999410d54637a3e5
liulawsi <liulawsi@gmail.com>
2020-12-15 14:26:56 +0800

    2020/12/15

--- Section04/BubbleSort/main.c
+++ Section04/BubbleSort/main.c
@@ -1,8 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <stdbool.h>
+
+ int main()
+ {
+     int n, temp, numbers[10];
+
+     while(1)
+     {
+         for(int i = 0; i < 5; i++)
+         {
+             printf("請輸入整數%d=", (i+1));
+             scanf("%d", &numbers[i]);
+         }
+
+         for(int i = 0; i < 4; i++)
+         {
+			for(int k = 0; k < 4; k++)
+			{
+				if (numbers[k] > numbers[k+1])
+				{
+					temp = numbers[k];
+					numbers[k] = numbers[k+1];
+					numbers[k+1] = temp;
+				}
+			}
+		 }
+		 for(int i = 0; i < 5; i++) {
+			 printf("%d,",numbers[i]);
+		 }
+		 printf("\n\n");
+     }
+     return 0;
+ }
+
